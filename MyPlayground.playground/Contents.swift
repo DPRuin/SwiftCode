b0042376cfe4b6d9eb8d542416d3ce5998904165
DPRuin <dpruin@126.com>
2018-01-30 12:12:40 +0800

    guard

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,21 +1,25 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-
-
-func doSomeWork {
+func doSomeWork() {
     print("getting started")
     defer {
         print("all done")
-        
     }
+    
     print("getting to work")
 }
 
 doSomeWork()
 
-
-
+func doSomeMoreWork() {
+    guard 2+2 == 4 else {
+        print("hhh")
+        return
+    }
+    print("ccc")
+}
+doSomeMoreWork()
 
 
 
