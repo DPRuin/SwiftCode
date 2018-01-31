9795031f99e98d8a2f7fca230ec6bbf089441648
DPRuin <dpruin@126.com>
2018-01-31 09:46:09 +0800

    sumNumbers

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -23,8 +23,15 @@ doSomeMoreWork()
 
 
 
+func sumNumbers(numbers: Int...) -> Int {
+    var total = 0
 
-
+    for number in numbers {
+        total += number
+    }
+    return total
+}
+sumNumbers(numbers: 1,2,3,4)
 
 
 
