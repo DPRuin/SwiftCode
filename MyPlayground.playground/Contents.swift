3e7a39933b7909a837af4da4739e427bd216a449
DPRuin <dpruin@126.com>
2018-02-03 22:28:05 +0800

    object

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,16 +2,14 @@
 
 import UIKit
 
-var numbers = [2,1,56,32,120,13]
-var numbersSorted = numbers.sorted { (n1, n2) -> Bool in
-    return n2 > n1
+class Vehicle {
+    var color: String?
+    
+    var maxSpeed = 80
+    
+
+    
 }
-
-numbers.sort()
-numbers
-
-var comparator = { (a:Int ,b:Int) in a < b}
-comparator(1, 2)
 
 
 
