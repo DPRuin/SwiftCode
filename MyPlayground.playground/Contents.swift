d5adda0e41d3b64c590a3651f1cd4cb74cecef71
DPRuin <dpruin@126.com>
2018-02-05 21:34:52 +0800

    object

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,8 +2,20 @@
 
 import UIKit
 
-
-
+// BEGIN inheritance
+class Car: Vehicle {
+    
+    var engineType : String = "V8"
+    
+    // BEGIN overidden_function
+    // Inherited classes can override functions
+    override func description() -> String  {
+        let description = super.description()
+        return description + ", which is a car"
+    }
+    // END overidden_function
+    
+}
 
 
 
